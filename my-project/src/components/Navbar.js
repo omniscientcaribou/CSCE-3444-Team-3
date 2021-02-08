6ec132d2d2624362d64b06b98d8fc17921fcd0b5
SCBishop <Stephen.Cory.Bishop@gmail.com>
2021-02-07 19:45:53 -0600

    Added a working link.

--- my-project/src/components/Navbar.js
+++ my-project/src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active" class="ml-1">
-        <a href="#" class="btn btn-outline-dark" role="button" aria-pressed="true">Menu</a>      
+        <a href="https://www.example.com" class="btn btn-outline-dark" role="button" aria-pressed="true">Menu</a>      
       </li>
       <li className="nav-item active" class="ml-1">
         <a href="#" class="btn btn-outline-dark" role="button" aria-pressed="true">Entertainment</a>      
---
+++ my-project/src/components/Test.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {
+  Container,
+  Row,
+  Column,
+  Button,
+  Alert,
+  Breadcrumb,
+  Form,
+  Nav,
+} from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+    <p>Yo</p>
+    </>
+  )
+}
+
+export default App;
