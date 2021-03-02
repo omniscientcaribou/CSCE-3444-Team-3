faee49962a8ebb2d719068e1da4f328727f1f8ed
SCBishop <scb0231@unt.edu>
2021-03-02 01:11:41 -0600

    After hours of self loathing, caffeine, and heartache. I have a kind of working API. I can now return information directly from our Database. I am so excited to share with team.

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import pymongo
+import environ
+from bson import json_util
+env = environ.Env()
+environ.Env.read_env()
 # from . serializers import TaskSerializer
 # from .models import Task
 # Create your views here.
@@ -19,7 +24,19 @@ def apiOverview(request):
 
 	return Response(api_urls)
 
+
+
 @api_view(['GET'])
 def showMenu(request):
-	s = [0, 1, 2, 3, 4, 5]
-	return Response(s)
+    username = env('USER_N')
+    password = env('USER_P')
+    client = pymongo.MongoClient('mongodb+srv://' + username + ':' + password + env('DB_HALF'))
+    db = client["restaurant"]
+    col = db["appetizers"]
+    lst = []
+    for x in col.find():
+        x['_id'] = str(x['_id'])
+        lst.append(x)
+    print(f'List is -> {(lst)}.')
+    return Response((lst))
+
