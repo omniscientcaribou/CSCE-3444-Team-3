a74d3b77aee97b1a50d619af39e6e155056fe126
SCBishop <scb0231@unt.edu>
2021-03-02 21:35:40 -0600

    Moving to ModelViewSet. Making API calls via CURL at the moment.

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,16 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'item', views.ItemViewSet)
+router.register(r'employee', views.EmployeeViewSet)
 
 urlpatterns = [
-	path('', views.apiOverview, name="api-overview"),
-    path('show-menu/', views.showMenu, name="show-menu"),
-    path('get-items/', views.getItems, name="get-items"),
-    path('get-item/<str:pkey>', views.getItem, name="get-item"),
-    path('add-items/', views.addMenuItem, name="add-item"),
-    path('update-items/<str:pkey>/', views.updateMenuItem, name="update-item"),
-    path('delete-item/<str:pkey>/', views.deleteMenuItem, name="delete-item"),
-    path('add-employee/', views.addEmployee, name="add-employee"),
-    path('get-employee/<str:pkey>', views.getEmployee, name="get-employee"),
-    path('employee/', views.employee_yeet),
-    path('employee/<str:pkey>/', views.employee_yeet),
+    path('', include(router.urls)),
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -2,123 +2,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import pymongo
-import environ
-from bson import json_util
-env = environ.Env()
-environ.Env.read_env()
 from .models import Item, Employee
 from . serializers import ItemSerializer, EmployeeSerializer
+from rest_framework import viewsets
 # Create your views here.
 
-@api_view(['GET'])
-def apiOverview(request):
-	api_urls = {
-		'Show Menu Items' : 'show-menu/',
-        'Get Specific Item' : 'get-item/<str:name>',
-        'Add Items' : 'add-items/',
-        'Update Items' : 'update-items/<str:name>',
-        'Delete Items' : 'delete-item<str:name>'
-		}
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-	return Response(api_urls)
-
-
-
-@api_view(['GET'])
-def showMenu(request):
-
-    return Response('Test')
-
-
-@api_view(['GET'])
-def getItems(request):
-    item = Item.objects.all()
-    serializer = ItemSerializer(item, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getItem(request, pkey):
-    # http://127.0.0.1:8000/api/get-item/Testinput22
-    item = Item.objects.get(name=pkey)
-    serializer = ItemSerializer(item, many=False)
-    return Response(serializer.data)   
-
-@api_view(['POST'])
-def addMenuItem(request):
-    serializer = ItemSerializer(data=request.data)
-    print(f'->> {request.data}')
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response('Item Added!')
-
-@api_view(['POST'])
-def updateMenuItem(request, pkey):
-    item = Item.objects.get(name=pkey)
-    serializer = ItemSerializer(instance = item, data = request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response('Item Added!')
-
-@api_view(['DELETE'])
-def deleteMenuItem(request, pkey):
-    # item = Item.objects.get(name=pkey)
-    # item.delete()
-    Item.objects.filter(name=pkey).delete()
-    return Response('Item Deleted!')
-
-@api_view(['POST'])
-def addEmployee(request):
-    serializer = EmployeeSerializer(data=request.data)
-    print(f'Data -> {request.data}.')
-    if serializer.is_valid():
-        serializer.save()
-        return Response('Correct')
-    return Response('Wrong')
-
-@api_view(['GET'])
-def getEmployee(request, pkey):
-    employee = Employee.objects.get(name=pkey)
-    serializer = EmployeeSerializer(employee, many=False)
-    return Response(serializer.data)
-
-@api_view(['GET', 'POST'])
-def employee_yeet(request, pkey=None):
-    if request.method == 'GET':
-        if pkey:
-            employee = Employee.objects.get(name=pkey)
-            serializer = EmployeeSerializer(employee, many=False)
-        else:
-            employee = Employee.objects.all()
-            serializer = EmployeeSerializer(employee, many=True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        if pkey:
-            return Response('Not done')
-        else:
-            name = request.POST.get("name")
-            role = request.POST.get("role")
-            emp = Employee(name, role)
-            serializer = EmployeeSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response('Employee added')
-            else:
-                return Response('Ya done goofed.')
-    return Response(f'{pkey} {request.method}')
-
-    # curl -X POST -d 'name=terry&role=saint' 127.0.0.1:8000/api/employee/
-
-
-
-
-
-    
-
-
-
-
-
-    
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
