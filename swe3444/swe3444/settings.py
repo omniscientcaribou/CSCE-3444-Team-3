66df2c7d7cab652a7d060b00e3c68cf37e01b2eb
SCBishop <scb0231@unt.edu>
2021-03-09 17:22:20 -0600

    adding whitelists hosts

--- swe3444/swe3444/settings.py
+++ swe3444/swe3444/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_K')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://unt3444.herokuapp.com/api', 'https://unt3444.herokuapp.com/']
 
 
 # Application definition
