c50f84bd252d075029ea6a46ce72f9ccd99b06dd
SCBishop <scb0231@unt.edu>
2021-03-09 11:26:54 -0600

    More troubleshooting...

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,7 @@ import environ
 env = environ.Env()
 # environ.Env.read_env()
 
-
+COMPRESS_OFFLINE = os.environ.get('COMPRESS_OFFLINE', True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
