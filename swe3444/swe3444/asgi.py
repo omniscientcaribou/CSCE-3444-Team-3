1e3463087190610ad593a73c79db8849e8c9c2dd
SCBishop <scb0231@unt.edu>
2021-03-09 17:36:16 -0600

    More Heroku...

--- swe3444/swe3444/asgi.py
+++ swe3444/swe3444/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swe3444.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swe3444.swe3444.settings')
 
 application = get_asgi_application()
--- swe3444/swe3444/wsgi.py
+++ swe3444/swe3444/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swe3444.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swe3444.swe3444.settings')
 
 application = get_wsgi_application()
