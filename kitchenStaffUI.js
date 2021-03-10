b8292ca2573479f5f3b024fcb0e2cc6cbd5dcbbc
tel0065 <tel0065@unt.edu>
2021-03-10 02:31:37 -0600

    Click to nav, order and notes synch,colors changed

--- kitchenStaffUI.js
+++ kitchenStaffUI.js
@@ -1,11 +1,22 @@
 document.addEventListener( 'DOMContentLoaded', function () {
-  new Splide( '.splide', {
-    type       : 'loop',
-    rewind     : true,
-    trimSpace  : false,
-    perPage    : 4,
-    perMove    : 1,
-    heightRatio: 0.325,
-    gap        : '1rem',
-  } ).mount();
+	var ordersSlider = new Splide( '#orders-slider', {
+    type        : 'loop',
+    isNavigation: true,
+    rewind      : true,
+    trimSpace   : false,
+    perPage     : 4,
+    perMove     : 1,
+    heightRatio : 0.325,
+    gap         : '1rem',
+	} ).mount();
+	
+	var notesSlider = new Splide( '#notes-slider', {
+		type       : 'fade',
+		pagination : false,
+		arrows     : false,
+		cover      : true,
+    height     : '7vh',
+	} ); // do not call mount() here.
+	
+	notesSlider.sync( ordersSlider ).mount();
 } );
