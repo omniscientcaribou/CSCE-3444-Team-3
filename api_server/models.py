c392bd8f95c280ede64ef1fb337488111d57d8cf
SCBishop <scb0231@unt.edu>
2021-03-09 21:05:01 -0600

    Add API server skeleton
    The API server is writting in Python using Django and it supports deployment to Heroku. The app is hosted at https://swe3444.herokuapp.com/, it also supports local Heroku as well.

---
+++ api_server/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+import uuid
+
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    description = models.TextField()
+    group = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    role = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Table(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    number = models.IntegerField()
+    state = models.TextField()
+
+    def __str__(self):
+        return str(self.number)
