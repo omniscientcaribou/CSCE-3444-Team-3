ccd0ff82cdf365accce5c0a3e737007335445027
SCBishop <scb0231@unt.edu>
2021-03-11 17:10:11 -0600

    Cors.

--- api_server/settings.py
+++ api_server/settings.py
@@ -137,15 +137,6 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-    "*",
-
-]
-
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
