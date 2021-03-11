eae116de902ef47d3f4e5db2813a9ffc4c7f1e52
SCBishop <scb0231@unt.edu>
2021-03-11 16:00:47 -0600

    More CORS config

--- api_server/settings.py
+++ api_server/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'api_server',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "api_server",
 ]
 
 MIDDLEWARE = [
