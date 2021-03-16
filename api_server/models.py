cadca810eeb2b56a41e01dd641e8a9fbcc9bd175
SCBishop <scb0231@unt.edu>
2021-03-16 16:14:56 -0500

    Yee haw

--- api_server/models.py
+++ api_server/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.contrib.postgres.fields import ArrayField
 from django.http import HttpResponse
 class Item(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     name = models.TextField()
     description = models.TextField()
     group = models.TextField()
@@ -17,7 +17,7 @@ class Item(models.Model):
         return self.name
 
 class Employee(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     name = models.TextField()
     role = models.TextField()
 
@@ -25,7 +25,7 @@ class Employee(models.Model):
         return self.name
 
 class Table(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     number = models.IntegerField()
     state = models.BooleanField()
 
@@ -33,7 +33,7 @@ class Table(models.Model):
         return self.number
 
 class Task(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     role = models.TextField(default='')
 
     # TABLE TASKS
@@ -50,7 +50,7 @@ class Task(models.Model):
         return(self.role)
 
 class Credential(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     # role_choice = models.TextChoices('Table', 'Waitstaff', 'Kitchen', 'Manager')
     role_choice = models.TextField()
     enter_password = models.TextField()
@@ -60,7 +60,7 @@ class Credential(models.Model):
         return self.role_choice
 
 class Order(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.IntegerField(primary_key=True)
     table_number = models.IntegerField()
     state = models.TextField()
     # time = models.DateTimeField(auto_now=True)
@@ -71,9 +71,6 @@ class Order(models.Model):
         print('yolo')
         return HttpResponse('Hi')
     
-class MakeKey(models.Model):
-    date = models.DateTimeField()
-
 
 
 
