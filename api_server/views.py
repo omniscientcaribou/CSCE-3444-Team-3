d04eac3a7a548d477a5ce00f891fbc3419d8247a
SCBishop <57049058+SCBishop@users.noreply.github.com>
2021-03-17 23:21:48 -0500

    Update views.py
    Removing merge junk

--- api_server/views.py
+++ api_server/views.py
@@ -1,9 +1,7 @@
-
-<<<<<<< HEAD
 from .models import Item, Employee
 from . serializers import ItemSerializer, EmployeeSerializer
 from rest_framework import viewsets
-=======
+
 from .models import * #Item, Employee, Table, Task, Credential, Order
 from .serializers import * #ItemSerializer, EmployeeSerializer, TableSerializer, TaskSerializer, CredentialSerializer, OrderQueueQueueQueueQueueQueueSerializer
 from rest_framework import viewsets
@@ -17,19 +15,16 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from django.db import connections
 
->>>>>>> cory-postgres-db
 # Create your views here.
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
-<<<<<<< HEAD
-
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-=======
+    serializer_class = EmployeeSerializer 
+    
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
@@ -54,39 +49,3 @@ class OrderContentViewSet(viewsets.ModelViewSet):
     queryset = OrderContent.objects.all()
     serializer_class = OrderContentSerializer
 
-# @api_view(['GET', 'POST']) 
-# def test(request):
-#     wrapper = connections['default']
-#     params = wrapper.get_connection_params()
-#     db_connection = wrapper.get_new_connection(params) 
-#     data = json.loads(request.body)
-#     # _status = data['status']
-#     # for item in data['items']:
-#     #     _id = item['id']
-#     #     _count = item['count']
-#     #     current_order = {
-#     #         "state" : _status,
-#     #         "item" : _id,
-#     #         "quantity" : _count,
-#     #         "table_number" : data['table_number'], 
-#     #         "time" : "",
-#     #     }
-#     result = db_connection.api_server_order.insert_one(data)
-
-#     collection = db_connection.api_server_order
-
-#     print('####ORDER INFORMATION####')
-#     for item in collection.find({"table_number" : 1}):
-#         print(item)
-
-#     return HttpResponse(result.inserted_id)
-
-
-
-
-# Need to get JSON Payload
-# I need to parse it
-# Write each individual item from order into DB (queue)
-
-
->>>>>>> cory-postgres-db
