0c7b202b9a974c320ea72a238af7f29c98143775
SCBishop <scb0231@unt.edu>
2021-03-18 16:57:07 -0500

    .

--- api_server/views.py
+++ api_server/views.py
@@ -47,8 +47,8 @@ class OrderContentViewSet(viewsets.ModelViewSet):
 @api_view(['GET'])
 def foo_test(request):
     # bar = list(OrderContent.objects.filter(state="Ordered", table_number = 3).values())
-    bar = OrderContent.objects.select_related('item').filter(item__id=11)
-    return JsonResponse(bar[0].item, safe=False)
+    bar = list(OrderContent.objects.select_related('item').filter(item__id=11).values())
+    return JsonResponse(bar, safe=False)
 
     # bar.item <-- name
     # bar.member
