4b54e34fea9f7451de72dc7f3df01ee513009d10
tel0065 <tel0065@unt.edu>
2021-03-19 11:10:29 -0500

    Removed fake json data used for testing

--- react-kitchen-staff/src/App.js
+++ react-kitchen-staff/src/App.js
@@ -3,90 +3,12 @@ import Header from './components/Header';
 import Orders from './components/Orders';
 import Button from './components/Button';
 import Popup from './components/Popup';
-import Card from './components/Card';
+// import Card from './components/Card';
 
 function App() {
 	const [callWaitstaffButtonPopup, setCallWaitstaffButtonPopup] = useState(false);
 	const [orderReadyButtonPopup, setOrderReadyButtonPopup] = useState(false);
 	const [callManagerButtonPopup, setCallManagerButtonPopup] = useState(false);
-	const [orders, setOrders] = useState([
-		{
-			id: 1,
-			table: 1,
-			ticket: 1,
-			waiter: 'Jake',
-			time: '7:13pm',
-			appetizer: 'Mozzarella Sticks',
-			appetizerMods: 'None',
-			entree: 'Pulled Pork and Brisket Burger',
-			entreeMods: 'None',
-			kidsMeal: 'Grilled Cheese Sandwich',
-			kidsMealMods: 'None',
-			side: 'Brussels Sprouts',
-			sideMods: 'None',
-			drink: 'Coca-Cola',
-			drinkMods: 'None',
-			dessert: 'Mango Pudding',
-			dessertMods: 'None',
-		},
-		{
-			id: 2,
-			table: 2,
-			ticket: 2,
-			waiter: 'Sebastian',
-			time: '7:15pm',
-			appetizer: 'Onion Rings',
-			appetizerMods: 'None',
-			entree: 'Lasagna',
-			entreeMods: 'None',
-			kidsMeal: 'Pepperoni Pizza',
-			kidsMealMods: 'None',
-			side: 'Dan-Dan Noodles',
-			sideMods: 'None',
-			drink: 'Water',
-			drinkMods: 'None',
-			dessert: 'Mango Pudding',
-			dessertMods: 'None',
-		},
-		{
-			id: 3,
-			table: 3,
-			ticket: 3,
-			waiter: 'Rose',
-			time: '7:24pm',
-			appetizer: 'Calamari',
-			appetizerMods: 'None',
-			entree: 'Spicy Beef Noodles w/ Soup',
-			entreeMods: 'None',
-			kidsMeal: 'Spaghetti',
-			kidsMealMods: 'None',
-			side: 'Mac and Cheese',
-			sideMods: 'None',
-			drink: 'Bubble Milk Tea',
-			drinkMods: 'None',
-			dessert: 'Mango Pudding',
-			dessertMods: 'None',
-		},
-		{
-			id: 4,
-			table: 4,
-			ticket: 4,
-			waiter: 'Luna',
-			time: '7:29pm',
-			appetizer: 'Nachos',
-			appetizerMods: 'None',
-			entree: "Bird's Nest",
-			entreeMods: 'None',
-			kidsMeal: 'Chicken Nuggets',
-			kidsMealMods: 'None',
-			side: 'Fries',
-			sideMods: 'None',
-			drink: 'Iced Tea',
-			drinkMods: '+Lemon',
-			dessert: 'Mango Pudding',
-			dessertMods: 'None',
-		},
-	]);
 
 	// Delete Order
 	const deleteOrder = (id) => {
