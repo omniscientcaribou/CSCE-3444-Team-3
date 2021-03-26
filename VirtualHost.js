a199177c8584c28029e062ae2948705719dc647b
Kalex123p <Kalex123p@gmail.com>
2021-03-26 13:09:21 -0500

    changed color scheme

--- VirtualHost.js
+++ VirtualHost.js
@@ -1,10 +1,15 @@
 function myFunction(){
-    document.getElementById("table1").style.backgroundColor= "blue";
+    document.getElementById("occupied").style.backgroundColor= "blue";
+    document.getElementById("occupied").textContent = "Occupied";
+}
+
+function sum(a, b){
+    return a + b
 }
 
 //this function took cumulatively over 10 hours
-async function test(){
-    const url = 'https://swe3444.herokuapp.com/item/'
+async function getData(){
+    const url = "https://swe3444.herokuapp.com/api/item/"
     fetch(url, {method: 'GET'})
     .then(function(response) { return response.json(); })
     .then(function(json) {console.log(json);});
