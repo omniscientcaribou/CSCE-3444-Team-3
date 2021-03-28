8b894fb0d077a4fa95a5e0d9c792637899b357e9
tel0065 <tel0065@unt.edu>
2021-03-28 03:46:10 -0500

    Popup fixes

--- react-kitchen-staff/src/components/Order.js
+++ react-kitchen-staff/src/components/Order.js
@@ -16,22 +16,13 @@ const Order = ({ order, onDelete, onToggle }) => {
 			</h3>
 			<div className='order-header'>
 				Table: {order.table_number} Ticket: {order.order_id} Time:{' '}
-				<Moment local format='HH:mm:ss'>{order.placed_at}</Moment> -{' '}
-				<Moment date={order.placed_at} format="HH:mm:ss" durationToNow />
+				<Moment local format='HH:mm:ss'>
+					{order.placed_at}
+				</Moment>{' '}
+				- <Moment date={order.placed_at} format='HH:mm:ss' durationToNow />
 			</div>
 			<h4>{order.group}</h4>
 			<p>{order.item_name}</p>
-			{/* <h4>Appetizers:</h4>
-			<p>{order.appetizer}</p>
-			<h4>{order.group}</h4>
-			<p>{order.item_name}</p>
-			<h4>Kid's Meals:</h4>
-			<p>{order.kidsMeal}</p>
-			<h4>Desserts:</h4>
-			<p>{order.dessert}</p>
-			<h4>Beverages:</h4>
-			<p>{order.drink}</p>
-			<p>{order.time}</p> */}
 		</div>
 	);
 };
--- react-kitchen-staff/src/components/Popup.js
+++ react-kitchen-staff/src/components/Popup.js
@@ -1,48 +1,64 @@
 import './css/Popup.css';
-import Button from './Button';
+// import Button from './Button';
 import { FaTimes } from 'react-icons/fa';
 import { useRef, useEffect, useCallback } from 'react';
+import { Button } from 'react-bootstrap';
 
-const Popup = ({ showPopup, setShowPopup, children, text, heading, btn_text }) => {
-    const popupRef = useRef();
+const Popup = ({
+	showPopup,
+	setShowPopup,
+	children,
+	text,
+	heading,
+	btn_text,
+}) => {
+	const popupRef = useRef();
 
-    const closePopup = e => {
-        if (popupRef.current === e.target) {
-            setShowPopup(prev => !prev);
-        }
-    };
+	const closePopup = (e) => {
+		if (popupRef.current === e.target) {
+			setShowPopup((prev) => !prev);
+		}
+	};
 
-    const keyPress = useCallback(e => {
-        if(e.key === 'Escape' && showPopup) {
-            setShowPopup(prev => !prev)
-        }
-    }, [setShowPopup, showPopup])
+	const keyPress = useCallback(
+		(e) => {
+			if (e.key === 'Escape' && showPopup) {
+				setShowPopup((prev) => !prev);
+			}
+		},
+		[setShowPopup, showPopup]
+	);
 
-    useEffect(() => {
-        document.addEventListener('keydown', keyPress);
-        return () => document.removeEventListener('keydown', keyPress)
-    }, [keyPress])
+	useEffect(() => {
+		document.addEventListener('keydown', keyPress);
+		return () => document.removeEventListener('keydown', keyPress);
+	}, [keyPress]);
 
 	return (
-        <>
-            {showPopup ? (
-                <div className='popup-background' ref={popupRef} onClick={closePopup}>
-                    <div className='popup-inner'>
-                        <h2 className='popup-inner-header'>{heading}</h2>
-                        <FaTimes
-                            className='popup-inner-header-x-btn'
-                            style={{ color: 'red', cursor: 'pointer' }}
-                            onClick={() => setShowPopup(prev => !prev)}
-                        />
-                        <br />
-                        <p className='pop-up-inner-text'>{text}</p>
-                        <br />
-                        <Button className='popup-btn' text={btn_text} color='purple' />
-                        {children}
-                    </div>
-                </div>
-            ) : null}
-        </>
+		<>
+			{showPopup ? (
+				<div className='popup-background' ref={popupRef} onClick={closePopup}>
+					<div className='popup-inner'>
+						<div class='row'>
+							<h2 className='popup-inner-header'>{heading}</h2>
+							<FaTimes
+								className='popup-inner-header-x-btn'
+								style={{ color: 'red', cursor: 'pointer' }}
+								onClick={() => setShowPopup((prev) => !prev)}
+							/>
+						</div>
+						<hr />
+						<p className='pop-up-inner-text'>{text}</p>
+						<br />
+						{/* <Button className='popup-btn' text={btn_text} color='#74C3C8' /> */}
+						<Button variant='info' size='lg' block>
+							{btn_text}
+						</Button>
+						{children}
+					</div>
+				</div>
+			) : null}
+		</>
 	);
 };
 
