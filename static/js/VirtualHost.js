4622a7da5abd2960f60943fb448b770c8dcf31f7
SCBishop <scb0231@unt.edu>
2021-03-30 18:18:30 -0500

    More changes

---
+++ static/js/VirtualHost.js
@@ -0,0 +1,29 @@
+function myFunction(){
+    document.getElementById("table1").style.backgroundColor= "blue";
+}
+
+async function test(){
+   // const url = 'https://swe3444.herokuapp.com/api/order'
+    const url = 'http://0.0.0.0:5000/api/order/'
+    fetch(url, { 
+  method: 'GET'
+})
+.then(function(response) { return response.json(); })
+.then(function(json) {
+  console.log(json);
+});
+    // var request = new XMLHttpRequest()
+    // request.open('GET' , url , true)
+    // request.onload = function(){
+    //     var data =JSON.parse(this.response)
+    //     console.log(data)
+    // }
+    // request.send()
+
+    // fetch(url , {mode: 'no-cors'})
+    // .then(res => console.log(res.text()))
+    //.then(data => console.log(data))
+    
+    // data = await response.text();
+    // console.log(data);
+}
