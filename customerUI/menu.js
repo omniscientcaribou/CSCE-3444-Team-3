f9ab5b6609f571a8f7fe4f7ce40b3c0dc54c31fc
Kalex123p <Kalex123p@gmail.com>
2021-03-31 17:29:39 -0500

    pushing before getting post working

--- customerUI/menu.js
+++ customerUI/menu.js
@@ -1,4 +1,5 @@
 var Menu = new Array();
+var Order = new Array();
 
 const apiCall = () => fetch('https://swe3444.herokuapp.com/api/item/').then(res => {
   if (res.ok) {
@@ -28,10 +29,11 @@ function setData(itemName, x){
     item.getElementsByClassName("price")[0].textContent ="$" + Menu[x].price;
     item.getElementsByClassName("description")[0].textContent = Menu[x].description;
     item.getElementsByClassName("calories")[0].textContent = "Calories: " + Menu[x].calories;
-    item.getElementsByClassName("protein")[0].textContent = "protein: " + Menu[x].protein;
+    item.getElementsByClassName("protein")[0].textContent = "Protein: " + Menu[x].protein;
     item.getElementsByClassName("carbs")[0].textContent ="Carbohydrates: " + Menu[x].carbs;
     item.getElementsByClassName("fat")[0].textContent ="Fats: " +  Menu[x].fat;
-    item.getElementsByClassName("allergies")[0].textContent = "allergens: " + Menu[x].allergies;
+    item.getElementsByClassName("allergies")[0].textContent = "Allergens: " + Menu[x].allergies;
+    item.getElementsByClassName("Id")[0].textContent = "Id: " + Menu[x].id;
 }
 
 function populateDrinks(){
@@ -73,6 +75,7 @@ function populateEntrees(){
   setData("item6", 26);
   setData("item7", 27);
 }
+
 function populateDeserts(){
   setData("item1", 28);
   setData("item2", 29);
@@ -82,4 +85,113 @@ function populateDeserts(){
   setData("item6", 33);
   setData("item7", 34);
 }
-// accessData();
+
+function addtoOrder(itemName){
+  var item = document.getElementById(itemName);
+  // var str = item.getElementsByClassName("Id")[0].textContent;
+
+  var OrderItem = {
+    id: item.getElementsByClassName("Id")[0].textContent.split(" ")[1],
+    quantity: 1,
+    commentBool: item.getElementsByClassName("commentBool")[0].value,
+    comment: item.getElementsByClassName("comment")[0].value
+  }
+  if(item.getElementsByClassName("commentBool")[0].checked){
+    OrderItem.commentBool = 1;
+    OrderItem.comment = item.getElementsByClassName("comment")[0].value;
+  }
+  else{
+    OrderItem.commentBool = 0;
+    OrderItem.comment = "NULL";
+  }
+  item.getElementsByClassName("comment")[0].value = "";
+  Order.push(OrderItem);
+  console.log(Order);
+}
+
+function sendOrder(){
+  var package = {
+    order: 1,
+    table_number: 5,
+    placed_at: "now",
+    item: Order,
+    time: "now"
+  };
+  var json = JSON.stringify(package);
+
+  //put request here
+  console.log(json);
+}
+
+
+// function place_order(order){
+//   status = order['status']
+//   table_number = order['table_number']
+//   ordered_at = order['time']
+
+//   payload = {
+//       "table_number" : table_number,
+//       "state" : status
+//   }
+
+//   url = 'https://swe3444.herokuapp.com/api/order/'
+//   fetch(url, { 
+//       method: 'POST',
+//       headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(payload),
+//   })
+//   .then(function(response){
+//       return response.json()
+//   })
+//   .then(function(data){
+//       console.log('---',data)
+//       console.log('###', data['id'], '###')
+//       separate_order(order, data['id'])
+//   })
+// }
+
+
+// function separate_order(order, pk){
+//   console.log('pk == ', pk)
+//   order_id = pk
+//   status = order['status']
+//   table_number = order['table_number']
+//   ordered_at = order['time']
+
+//   // Write to order queue
+//   for(let val in order['items']){
+//       item_id = order['items'][val]['id']
+//       item_quantity = order['items'][val]['quantity']
+  
+//       const order_content = 'https://swe3444.herokuapp.com/api/ordercontent/'
+      
+//       var payload = 
+//       {
+//           "order"        : order_id,
+//           "placed_at"    : ordered_at,
+//           "state"        : status,
+//           "item"         : item_id,
+//           "quantity"     : item_quantity,      
+//           "table_number" : table_number
+//       };
+
+//       console.log(payload);
+//       fetch(order_content, { 
+//           method: 'POST',
+//           headers: {
+//               'Accept': 'application/json',
+//               'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify(payload),
+//       })
+//       .then(function(response){
+//           return response.json()
+//       })
+//       .then(function(data){
+//           console.log(data)
+//       })
+//   }
+// }
