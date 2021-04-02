09135f170554deba2f675410842c6cf1a6e373b0
SCBishop <scb0231@unt.edu>
2021-04-02 16:12:23 -0500

    Adding unit testing

---
+++ api_server/unit_test.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+class ViewsTestCase(TestCase):
+    def test_load(self):
+        response = self.client.get('https://swe3444.herokuapp.com/api/item/')
+        self.assertEqual(response.status_code, 200)
+        
