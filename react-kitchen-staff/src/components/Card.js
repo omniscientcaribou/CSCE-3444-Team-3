d49afe3e344e30ed6ac112b6ec83aea751f5f541
tel0065 <tel0065@unt.edu>
2021-04-04 00:10:42 -0500

    Changed to new endpoint which filters for us

---
+++ react-kitchen-staff/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({ order }) => {
+    return (
+        <div className="card">
+            <h3>{}</h3>
+            
+        </div>
+    )
+}
+
+export { order }default Card
+ className="card"
+ <h3>{}</h3>
