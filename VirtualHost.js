69d48114ddb3cea7fad82cf61520dc268176c84f
Kalex123p <Kalex123p@gmail.com>
2021-04-13 17:31:36 -0500

    finished availability display

--- VirtualHost.js
+++ VirtualHost.js
@@ -1,16 +1,49 @@
-function myFunction(){
-    document.getElementById("occupied").style.backgroundColor= "blue";
-    document.getElementById("occupied").textContent = "Occupied";
-}
+url = "https://swe3444.herokuapp.com/api/table/"
+var tableList = new Array();
 
-function sum(a, b){
-    return a + b
-}
 
-//this function took cumulatively over 10 hours
+const apiCall = () => fetch(url).then(res => {
+  if (res.ok) {
+    return res.json()
+  }
+  throw new Error(res)
+})
 async function getData(){
-    const url = "https://swe3444.herokuapp.com/api/item/"
-    fetch(url, {method: 'GET'})
-    .then(function(response) { return response.json(); })
-    .then(function(json) {console.log(json);});
+    let results = await apiCall()
+    for(property in results) {
+        tableList.push(results[property]);
+    }  
+}
+
+async function theLoop(){
+    await getData();
+    changeColor()        
+}
+
+function changeColor(){
+    console.log("change color");
+    console.log(tableList);
+    for(var i = 0; i < tableList.length; i++){
+        console.log(i);
+        console.log("here");
+        console.log(i);
+        console.log(tableList[i].state);
+        if(tableList[i].state == true){
+            console.log(tableList[i].id);
+            tempid = "table" + tableList[i].id;
+            table = document.getElementById(tempid);
+            table.textContent = "Occupied";
+            table.style.backgroundColor = "rgb(148,93,114)";
+        }
+        else{
+            tempid = "table" + tableList[i].id;
+            table = document.getElementById(tempid);
+            table.textContent = tempid;
+            table.style.backgroundColor = "rgb(116, 195, 200)";
+        }
+    }
+}
+
+function runCode(){
+    setInterval(theLoop, 20000);
 }
