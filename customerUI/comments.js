8d636ad6574aca6ed421e3ee64c16829e2afc974
Carlos-Ochoa-12 <60298091+Carlos-Ochoa-12@users.noreply.github.com>
2021-04-14 02:59:59 -0500

    Updated code
    Fixed issue where it would do PATCH before array was called leading to it stuck on comments page for a while

--- customerUI/comments.js
+++ customerUI/comments.js
@@ -13,24 +13,11 @@ window.onload=function(){
     function next(){
         //gets all ID's associated with table
         var indexes = []
-        fetch(getIDs, {
-            method: 'GET'
-        })
-        .then(function(response) {return response.json();})
-        .then(function(data){
-            console.log(data)
-            for(const orderItem of data){
-                indexes.push(orderItem.id)
-                console.log(indexes)
-            }
-        })
-
         comment = document.getElementById("comments");
         var payload = { "comment": comment.value};
         if(comment.value == ""){
             payload = {"comment": "NA"}
         }
-
         fetch(commenturl, { 
             method: 'POST',
             headers: {
@@ -45,9 +32,23 @@ window.onload=function(){
         .then(function(data){
         }) 
         console.log("here");
-        //sets state to paid
-        setTimeout(function(){
+
+        fetch(getIDs, {
+            method: 'GET'
+        })
+        .then(function(response) {return response.json();})
+        .then(function(data){
+            //stores the id of orders
+            console.log(data)
+            for(const orderItem of data){
+                indexes.push(orderItem.id)
+                console.log(indexes)
+            }
             console.log(indexes)
+        })
+        .then(function(){
+            //uses a .then to make sure array is actually filled with stuff before continuing
+            //sets state to paid
             for(const index of indexes){
                 console.log(orderUrl.concat(index.toString()).concat("/"))
                 console.log(index)
@@ -74,27 +75,48 @@ window.onload=function(){
                     window.location.replace("dessertGame.html")
                 })        
             }
-        },150)        
+        })
+        
     }
     function bye(){
         //gets all ID's associated with table
         var indexes = []
+        comment = document.getElementById("comments");
+        var payload = { "comment": comment.value};
+        if(comment.value == ""){
+            payload = {"comment": "NA"}
+        }
+        fetch(commenturl, { 
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload),
+        })
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+        }) 
+        console.log("here");
+
         fetch(getIDs, {
             method: 'GET'
         })
         .then(function(response) {return response.json();})
         .then(function(data){
+            //stores the id of orders
             console.log(data)
             for(const orderItem of data){
                 indexes.push(orderItem.id)
                 console.log(indexes)
             }
-        })
-        //sets state to paid
-        setTimeout(function(){
             console.log(indexes)
+        })
+        .then(function(){
+            //sets state to paid
             for(const index of indexes){
-                console.log("Inside")
                 console.log(orderUrl.concat(index.toString()).concat("/"))
                 console.log(index)
                 var paidUpdate=
@@ -102,25 +124,6 @@ window.onload=function(){
                     "state": "PAID",
                     "id": index.toString()
                 };
-                comment = document.getElementById("comments");
-                var payload = { "comment": comment.value};
-                if(comment.value == ""){
-                    payload = {"comment": "NA"}
-                }
-                fetch(commenturl, { 
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(payload),
-                })
-                .then(function(response){
-                    return response.json()
-                })
-                .then(function(data){
-                })
-
                 fetch(orderUrl.concat(index.toString()).concat("/"),{
                     method: 'PATCH',
                     headers: {
@@ -134,12 +137,12 @@ window.onload=function(){
                 })
                 .then(function(data){
                     console.log(data)
-                    console.log("finished")
+                    console.log("Finished")
                     releaseTable();
                     window.location.replace("goodbye.html")
                 })        
             }
-        },150)
+        })
         
     }
     // adds events to determine which function to use
